3b38030dafcaaa0b25b7627e33df794933b9aa4d
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-16 12:59:47 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
     has_many :prototypes
+<<<<<<< Updated upstream
                               has_many :comments
+=======
+     has_many :comments
+>>>>>>> Stashed changes
 
 
          validates :name, presence: true
